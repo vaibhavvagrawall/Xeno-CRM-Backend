44d366f480f40b60ec45079a340c334173ee59c6
vaibhavvagrawall <vaibhav.18.agrawal@gmail.com>
2024-11-13 04:53:25 +0530

    auth google ang server

--- server.js
+++ server.js
@@ -3,21 +3,36 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require('cors');
 const bodyParser = require('body-parser')
-const connectDB = require("./config/db");
-const customerRoutes = require('./routes/customerRoutes');
-const orderRoutes = require('./routes/orderRoutes');
+const passport = require('passport');
+const session = require('express-session');
 
 dotenv.config();
+
+const connectDB = require("./config/db");
+const auth = require('./middleware/auth');
+
+const customerRoutes = require('./routes/customerRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json()); 
 
+app.use(session({
+  secret: 'secret', 
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false } // Use `secure: true` in production with HTTPS
+}));
+
+// Initialize passport and passport session
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get("/", (req, res) => {
   res.send("Server is connected");
 });
-
 
 app.use('/api', customerRoutes);
 app.use('/api', orderRoutes);
@@ -26,4 +41,22 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   connectDB();
+});
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }),(req, res) => {
+  res.redirect('/profile');
+});
+
+app.get('/profile', (req, res) =>{
+  res.send(`Welcome ${req.user.displayName}`);
+});
+
+app.get('/logout', (req, res) => {
+  req.logout(() => {
+    res.redirect('/');
+});
 });

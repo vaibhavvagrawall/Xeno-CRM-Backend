908e4e7d77aeddc3df9c38e6c180e2cf00cf6ccb
vaibhavvagrawall <vaibhav.18.agrawal@gmail.com>
2024-11-12 22:43:27 +0530

    audience creation

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -18,3 +18,86 @@ exports.getCustomer = async (req,res) =>{
         res.status(500).json({error: error.message});
     }
 };
+
+exports.getHighSpendingCustomers = async (req, res) =>{
+    try {
+        const customers = await Customer.find({ totalSpending: { $gt: 10000 } });
+        res.json(customers);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getLowSpendingCustomers = async (req, res) =>{
+    try {
+        const customers = await Customer.find({ totalSpending: { $lte: 10000 } });
+        res.json(customers);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getHighVisitsCustomers = async (req, res) =>{
+    try {
+        const customers = await Customer.find({ visitCount: { $gt: 3 } });
+        res.json(customers);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getLowVisitsCustomers = async (req, res) =>{
+    try {
+        const customers = await Customer.find({ visitCount: { $lte: 3 } });
+        res.json(customers);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getHighSpendingHighVisitCustomers = async (req, res) =>{
+    try {
+        const customers = await Customer.find({ $and: [{ totalSpending: { $gt: 10000 }, visitCount: { gt: 3 } }] });
+        res.json(customers);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getHighSpendingLowVisitCustomers = async (req, res) =>{
+    try {
+        const customers = await Customer.find({ $and: [{ totalSpending: { $gt: 10000 }, visitCount: { $lte: 3 } }] });
+        res.json(customers);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getLowSpendingHighVisitCustomers = async (req, res) =>{
+    try {
+        const customers = await Customer.find({ $and: [{ totalSpending: { $lte: 10000 }, visitCount: { $gt: 3 } }] });
+        res.json(customers);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getLowSpendingLowVisitCustomers = async (req, res) =>{
+    try {
+        const customers = await Customer.find({ $and: [{ totalSpending: { $lte: 10000 }, visitCount: { $lte: 3 } }] });
+        res.json(customers);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getInactiveCustomers = async (req, res) => {
+    try {
+        const threeMonthsAgo = new Date();
+        threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3);
+        const customers = await Customer.find({ lastVisit: { $lt: threeMonthsAgo } });
+        res.status(200).json(customers);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};

3366958a989cd900cc7a880cb6cb0960af6bb77a
vaibhavvagrawall <vaibhav.18.agrawal@gmail.com>
2024-11-17 21:37:28 +0530

    miscellaneous

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-  
+ 
 passport.use(
     new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
